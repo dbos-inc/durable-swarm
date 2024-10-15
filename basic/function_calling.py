dd8d8e2afaf16bdf8a5ea00488b4f1866869625f
Qian Li <qian.li@dbos.dev>
2024-10-15 15:59:30 -0700

    Complete basic examples

---
+++ basic/function_calling.py
@@ -0,0 +1,20 @@
+from swarm import Agent
+from durable_swarm import DurableSwarm
+
+client = DurableSwarm()
+
+
+def get_weather(location) -> str:
+    return "{'temp':67, 'unit':'F'}"
+
+
+agent = Agent(
+    name="Agent",
+    instructions="You are a helpful agent.",
+    functions=[get_weather],
+)
+
+messages = [{"role": "user", "content": "What's the weather in NYC?"}]
+
+response = client.run(agent=agent, messages=messages)
+print(response.messages[-1]["content"])
