d9615362edaec90eb1561ed5072f6176ce4a87d7
Qian Li <qian.li@dbos.dev>
2024-10-15 18:04:37 -0700

    Simplify durable_swarm util functions

--- examples/weather_agent/durable_swarm.py
+++ examples/weather_agent/durable_swarm.py
@@ -1,6 +1,6 @@
-import json
 from swarm import Swarm
 from dbos import DBOS, DBOSConfiguredInstance
+from swarm.repl.repl import process_and_print_streaming_response, pretty_print_messages
 
 DBOS()
 
@@ -25,14 +25,20 @@ class DurableSwarm(Swarm, DBOSConfiguredInstance):
 DBOS.launch()
 
 #####################################
-# Util functions to run the CLI loop
+# Util function to run the CLI loop, modified for Durable Swarm
+# Original code: https://github.com/openai/swarm/blob/main/swarm/repl/repl.py
 #####################################
 
 def run_demo_loop(
     starting_agent, context_variables=None, stream=False, debug=False
 ) -> None:
-    client = DurableSwarm()
-    print("Starting Durable Swarm CLI 🐝")
+    if stream:
+        # If streaming, we need to use the non-durable client
+        client = Swarm()
+        print("Streaming not supported with Durable Swarm, using regular Swarm CLI 🐝")
+    else:
+        client = DurableSwarm()
+        print("Starting Durable Swarm CLI 💪🐝")
 
     messages = []
     agent = starting_agent
@@ -56,56 +62,3 @@ def run_demo_loop(
 
         messages.extend(response.messages)
         agent = response.agent
-
-def process_and_print_streaming_response(response):
-    content = ""
-    last_sender = ""
-    for chunk in response:
-        print(chunk)
-        if "sender" in chunk:
-            last_sender = chunk["sender"]
-
-        if "content" in chunk and chunk["content"] is not None:
-            if not content and last_sender:
-                print(f"\033[94m{last_sender}:\033[0m", end=" ", flush=True)
-                last_sender = ""
-            print(chunk["content"], end="", flush=True)
-            content += chunk["content"]
-
-        if "tool_calls" in chunk and chunk["tool_calls"] is not None:
-            for tool_call in chunk["tool_calls"]:
-                f = tool_call["function"]
-                name = f["name"]
-                if not name:
-                    continue
-                print(f"\033[94m{last_sender}: \033[95m{name}\033[0m()")
-
-        if "delim" in chunk and chunk["delim"] == "end" and content:
-            print()  # End of response message
-            content = ""
-
-        if "response" in chunk:
-            return chunk["response"]
-
-
-def pretty_print_messages(messages) -> None:
-    for message in messages:
-        if message["role"] != "assistant":
-            continue
-
-        # print agent name in blue
-        print(f"\033[94m{message['sender']}\033[0m:", end=" ")
-
-        # print response, if any
-        if message["content"]:
-            print(message["content"])
-
-        # print tool calls in purple, if any
-        tool_calls = message.get("tool_calls") or []
-        if len(tool_calls) > 1:
-            print()
-        for tool_call in tool_calls:
-            f = tool_call["function"]
-            name, args = f["name"], f["arguments"]
-            arg_str = json.dumps(json.loads(args)).replace(":", "=")
-            print(f"\033[95m{name}\033[0m({arg_str[1:-1]})")
